d3ecf053c875b7a4a8a395c407cff06af94e9798
VasjaVn <vp@winstars.tech>
2020-01-25 10:56:24 +0200

    Fill enum QualityGrade values from specification

--- src/main/java/refinitiv/scdadlsvc/dao/entity/enums/QualityGrade.java
+++ src/main/java/refinitiv/scdadlsvc/dao/entity/enums/QualityGrade.java
@@ -1,7 +1,10 @@
 package refinitiv.scdadlsvc.dao.entity.enums;
 
 public enum QualityGrade {
-    PRODUCT,
-    QUALITY_GRADLE_1,
-    QUALITY_GRADLE_2
+    PRODUCTION,
+    PRE_PRODUCTION,
+    INTERGATION_TESTING,
+    QUALITY_ASSURANCE,
+    DEVELOPMENT,
+    LAB
 }
