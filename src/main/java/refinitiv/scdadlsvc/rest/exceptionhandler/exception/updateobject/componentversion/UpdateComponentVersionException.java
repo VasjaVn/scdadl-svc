8fb11842c74899a2702b5160aaccc59114862c28
VasjaVn <VasjaVn>
2020-03-11 15:03:36 +0200

    remove exception UpdateComponentVersionException

--- src/main/java/refinitiv/scdadlsvc/rest/exceptionhandler/exception/updateobject/componentversion/UpdateComponentVersionException.java
+++
@@ -1,9 +0,0 @@
-package refinitiv.scdadlsvc.rest.exceptionhandler.exception.updateobject.componentversion;
-
-import refinitiv.scdadlsvc.rest.exceptionhandler.exception.updateobject.UpdateScdadlObjectException;
-
-public class UpdateComponentVersionException extends UpdateScdadlObjectException {
-    public UpdateComponentVersionException(String message) {
-        super(message);
-    }
-}
