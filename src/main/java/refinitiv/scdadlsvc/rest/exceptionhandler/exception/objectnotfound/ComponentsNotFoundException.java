9ea5ef1c67d877aa7a02c8f8abf1d8bf76376ef6
VasjaVn <vp@winstars.tech>
2020-03-11 05:54:31 +0200

    add exception ComponentsNotFoundException

---
+++ src/main/java/refinitiv/scdadlsvc/rest/exceptionhandler/exception/objectnotfound/ComponentsNotFoundException.java
@@ -0,0 +1,7 @@
+package refinitiv.scdadlsvc.rest.exceptionhandler.exception.objectnotfound;
+
+public class ComponentsNotFoundException extends ScdadlObjectNotFoundException {
+    public ComponentsNotFoundException(String message) {
+        super(message);
+    }
+}
