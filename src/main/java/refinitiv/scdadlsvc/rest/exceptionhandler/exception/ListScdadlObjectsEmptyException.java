03e48d09587a1e825b5cf7f061b5510d1d44fe8f
VasjaVn <VasjaVn>
2020-03-12 15:58:52 +0200

    add exception ListScdadlObjectsEmptyException

---
+++ src/main/java/refinitiv/scdadlsvc/rest/exceptionhandler/exception/ListScdadlObjectsEmptyException.java
@@ -0,0 +1,7 @@
+package refinitiv.scdadlsvc.rest.exceptionhandler.exception;
+
+public class ListScdadlObjectsEmptyException extends RuntimeException {
+    public ListScdadlObjectsEmptyException(String message) {
+        super(message);
+    }
+}
